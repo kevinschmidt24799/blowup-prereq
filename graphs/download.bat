ef01c9cbfa5745b63584ba22cd268e8af561cc35
kevinschmidt24799 <kevin@schmidtparty.com>
2025-09-16 20:48:17 -0700

    download

---
+++ graphs/download.bat
@@ -0,0 +1,11 @@
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph2.g6
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph3.g6
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph4.g6
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph5.g6
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph6.g6
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph7.g6
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph8.g6
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph9.g6
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph10.g6.gz
+curl -O https://users.cecs.anu.edu.au/~bdm/data/graph11.is6.gz
+
--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 
+n = 9
+count = 0
 
-G = nx.complete_graph(5)
-H = nx.path_graph(2)
-
-P = nx.tensor_product(G,H)
-nx.draw(P)
-plt.show()
+G = nx.read_graph6(f"graphs/graph{n}.g6")
+for g in G:
+    arr = nx.adjacency_spectrum(g)
+    arr.sort()
+    v = (arr[n-3].real+1)/n
+    if v >= 1/3.001:
+        print(arr)
+        count += 1
+        print(g)
+        print(v)
+        nx.draw(g)
+        plt.show()
+print (count)
